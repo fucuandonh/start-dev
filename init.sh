345c30758b32292abb5881b860c0b3143b7b179e
fucuandonh <fcuandonh@gmail.com>
2024-10-30 17:42:17 -0600

    feat: refresh bash environments

--- init.sh
+++ init.sh
@@ -1,5 +1,16 @@
-mkdir ~/.backup
-mv ~/{.bash_aliases,.profile,.bashrc} ~/.backup/
-cp bash/** ~/
+if [ ! -d ~/.backup ] ; then
+    mkdir ~/.backup
+fi
+
+main_files=(.bash_aliases .profile .bashrc)
+for file in "${main_files[@]}"; do
+    if [ -f ~/$file ] ; then
+        mv ~/$file ~/.backup/ 
+    fi
+done
+
 cp -r .m2/ ~/
 cp -r .util/ ~/
+cp -r bash/.[!.]* ~/
+
+source ~/.bashrc
